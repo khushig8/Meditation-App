65dae03d480468b15c4ae4860029c31a5fc18111
khushig8 <khushigarg9785@gmail.com>
2025-01-27 18:24:13 +0530

    Audio player

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -4,10 +4,10 @@ import "../../global.css"
 export default function RootLayout() {
     return (
         <Stack>
-            <Stack.Screen name="index" options={{ title: 'Meditation' }} />
+            <Stack.Screen name="index" options={{ title: 'Meditations' }} />
             <Stack.Screen
                 name="meditation/[id]"
-                options={{ headerShown: false, animation: "slide_from_bottom" }}
+                options={{ headerShown: false, animation: 'slide_from_bottom' }}
             />
         </Stack>
     );
